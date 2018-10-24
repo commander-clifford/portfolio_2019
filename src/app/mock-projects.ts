2c0a43aa8b15e9552ba0b30da1996ca8d990b4f1
Clifford Nelson <cliffordn@google.com>
2018-10-23 23:14:34 -0700

    comma comma comma comma comma comma-ellion, you come and go

--- src/app/mock-projects.ts
+++ src/app/mock-projects.ts
@@ -19,7 +19,7 @@ export const PROJECTS: Project[] = [
       brief: 'Prototyping',
       elaborate: ['Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',]
     },
-  }
+  },
   // Windshield Wipers
   {
     id: 1,
@@ -37,8 +37,7 @@ export const PROJECTS: Project[] = [
       brief: 'Prototyping playability',
       elaborate: ['Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',]
     },
-  }
-
+  },
 
   // TurboTax
 
